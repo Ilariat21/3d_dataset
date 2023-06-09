06a2bed367af07aaea041ad563896e1af602eb04
Tairali Assylbekov <73842362+Ilariat21@users.noreply.github.com>
2023-06-09 19:56:21 +0900

    Update pc_view.py

--- pc_view.py
+++ pc_view.py
@@ -1,18 +1,32 @@
-
 import open3d as o3d
 import os
 
-path = "New_Dataset/filled_cube.off"
+# Indicate path to the point cloud
+path = "Path-to-point-cloud"
 
 def vis_mesh(path):
-    if path[-3:]=="ply" or path[-3:]=="pcd" or path[-3:]=="off":
+    # Check the file extension to determine the type of mesh
+    if path[-3:] == "ply" or path[-3:] == "pcd" or path[-3:] == "off":
+        # Read the point cloud data
         mesh = o3d.io.read_point_cloud(path)
-    elif path[-3:]=="obj":
+    elif path[-3:] == "obj":
+        # Read the triangle mesh data
         mesh = o3d.io.read_triangle_mesh(path)
-    
+    else:
+        # Unsupported file format
+        print("Unsupported file format.")
+        return
+
+    # Print the mesh object
     print(mesh)
 
+    # Visualize the mesh using Open3D's visualization module
     o3d.visualization.draw_geometries([mesh])
 
-vis_mesh(path)
+def main():
+    # Call the vis_mesh function with the specified path
+    vis_mesh(path)
 
+if __name__ == "__main__":
+    # Execute the main function when the script is run directly
+    main()
