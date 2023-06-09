116efe1feb846d1dbb33882b856d408a648731ff
Tairali Assylbekov <73842362+Ilariat21@users.noreply.github.com>
2023-06-09 20:24:22 +0900

    Update fill_rectangle.py

--- fill_rectangle.py
+++ fill_rectangle.py
@@ -16,46 +16,52 @@ def check(path):
     if not os.path.exists(path):
         os.mkdir(path)
 
-inc = 0  # Counter to keep track of modified obj files
+def main():
+    check(path)
 
-filename = os.listdir(path)
+    inc = 0  # Counter to keep track of modified obj files
 
-for file_name in filename:
-    mesh_new = o3d.io.read_triangle_mesh(f"{path}/{file_name}")
+    filename = os.listdir(path)
 
-    f = open(f"{path}/{file_name}", "r")
-    lines = f.readlines()
-    vertices = []
-    faces = []
-    for line in lines:
-        if line[0] == "v":
-            # Append existing vertices to the list
-            vertices.append(line)
-        elif line[0] == "f":
-            # Append existing faces to the list
-            faces.append(line)
+    for file_name in filename:
+        mesh_new = o3d.io.read_triangle_mesh(f"{path}/{file_name}")
 
-    # Extract the bounding box coordinates of the mesh
-    x1, x2 = np.float64((np.min(np.asarray(mesh_new.vertices)[:, 0]))), np.float64(
-        (np.max(np.asarray(mesh_new.vertices)[:, 0])))
-    y1, y2 = np.float64((np.min(np.asarray(mesh_new.vertices)[:, 1]))), np.float64(
-        (np.max(np.asarray(mesh_new.vertices)[:, 1])))
-    z1, z2 = np.float64((np.max(np.asarray(mesh_new.vertices)[:, 2]))), np.float64(
-        (np.min(np.asarray(mesh_new.vertices)[:, 2])))
+        f = open(f"{path}/{file_name}", "r")
+        lines = f.readlines()
+        vertices = []
+        faces = []
+        for line in lines:
+            if line[0] == "v":
+                # Append existing vertices to the list
+                vertices.append(line)
+            elif line[0] == "f":
+                # Append existing faces to the list
+                faces.append(line)
 
-    # Add random vertices to reach the desired total number of vertices
-    while len(vertices) < num_points:
-        vertices.append(
-            f"v {random.uniform(x1, x2)} {random.uniform(y1, y2)} {random.uniform(z1, z2)}\n"
-        )
+        # Extract the bounding box coordinates of the mesh
+        x1, x2 = np.float64((np.min(np.asarray(mesh_new.vertices)[:, 0]))), np.float64(
+            (np.max(np.asarray(mesh_new.vertices)[:, 0])))
+        y1, y2 = np.float64((np.min(np.asarray(mesh_new.vertices)[:, 1]))), np.float64(
+            (np.max(np.asarray(mesh_new.vertices)[:, 1])))
+        z1, z2 = np.float64((np.max(np.asarray(mesh_new.vertices)[:, 2]))), np.float64(
+            (np.min(np.asarray(mesh_new.vertices)[:, 2])))
 
-    ff = open(f"{out_path}/res_rect_{inc}.obj", "w")
-    for v in vertices:
-        ff.write(v)
-    for face in faces:
-        ff.write(face)
+        # Add random vertices to reach the desired total number of vertices
+        while len(vertices) < num_points:
+            vertices.append(
+                f"v {random.uniform(x1, x2)} {random.uniform(y1, y2)} {random.uniform(z1, z2)}\n"
+            )
 
-    print(f"res_rect_{inc}.obj is done")
-    inc += 1  # Increment the counter for the next modified obj file
+        ff = open(f"{out_path}/res_rect_{inc}.obj", "w")
+        for v in vertices:
+            ff.write(v)
+        for face in faces:
+            ff.write(face)
 
-print("Finished!")
+        print(f"res_rect_{inc}.obj is done")
+        inc += 1  # Increment the counter for the next modified obj file
+
+    print("Finished!")
+
+if __name__ == "__main__":
+    main()
